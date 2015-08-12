b52b4504d0e1d9a83615e7c9d05228f7b07b9ccf
Leonidas Tsampros <ltsampros@gmail.com>
2015-08-12 10:24:13 +0300

    App returns the hostname of the server.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from socket import gethostname
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! I'm server: " + gethostname()
 
 if __name__ == "__main__":
     app.run()
